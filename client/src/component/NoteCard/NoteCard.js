531addaf9a16f3d04385b223f8ee7ee1a1dc72a1
ankitapawar0608 <kedarpawar775@gmail.com>
2024-04-04 06:56:09 +0530

    add new btn added

--- client/src/component/NoteCard/NoteCard.js
+++ client/src/component/NoteCard/NoteCard.js
@@ -3,41 +3,45 @@ import "./Note.css"
 import Bin from "./trash.png"
 import Edit from "./edit.png"
 import axios from 'axios'
- import toast from 'react-hot-toast'
+import toast from 'react-hot-toast'
 
 import { Link } from 'react-router-dom'
 
 
-function NoteCard({ _id, title, content, category ,loadNotes}) {
+function NoteCard({ _id, title, content, category, loadNotes }) {
 
   const deleteNote = async () => {
     const response = await axios.delete(`${process.env.REACT_APP_API_URL}/notes/${_id}`)
-    
-     toast.success(response.data.message)
+
+    toast.success(response.data.message)
     loadNotes()
   }
   return (
+    <>
+  
+
+
     <div className='container'>
 
-     
-       <div className='note-card'>
-      <h3 className='note-card-title'>{title}</h3>
-      <p className='note-card-content'>{content}</p>
-      <span className='note-card-category'>{category}</span>
-     <img src={Bin}
-        alt='delete-icon'
-        className='delete-icon'
-        onClick={deleteNote} />
+      <div className='note-card'>
+        <h3 className='note-card-title'>{title}</h3>
+        <p className='note-card-content'>{content}</p>
+        <span className='note-card-category'>{category}</span>
+        <img src={Bin}
+          alt='delete-icon'
+          className='delete-icon'
+          onClick={deleteNote} />
 
-      <Link to={`/update/${_id}`}>
-        <img src={Edit}
-          alt='update-icon'
-          className='update-icon' />
-      </Link>
+        <Link to={`/update/${_id}`}>
+          <img src={Edit}
+            alt='update-icon'
+            className='update-icon' />
+        </Link>
+      </div>
     </div>
-    </div>
+    </>
 
-   
+
   )
 }
 
--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import axios from "axios";
 import "./Home.css"
 import NoteCard from '../../component/NoteCard/NoteCard';
+import { Link } from 'react-router-dom';
 
 function Home() {
     const [notes, setNotes] = useState([])
@@ -18,13 +19,18 @@ function Home() {
     return (
         <div>
             <h1 className='text-center text-light m-5'>All Notes</h1>
+            <div className='text-center'>
+            <Link to={`/new`}>
+                <button className='text-center btn btn-light mb-3'>Add New Note</button>
+            </Link>
+            </div>
             {
-                notes.map((note)=>{
-                    const {_id, title,content,category}=note
-                   return(
-                  <NoteCard key={_id} _id={_id} title={title} content={content}
-                   category={category} loadNotes={loadNotes}/>
-                   )
+                notes.map((note) => {
+                    const { _id, title, content, category } = note
+                    return (
+                        <NoteCard key={_id} _id={_id} title={title} content={content}
+                            category={category} loadNotes={loadNotes} />
+                    )
                 })
             }
         </div>
